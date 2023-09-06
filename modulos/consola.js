414c9205ae830e07f1ae9cde7a5282386fbf714e
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-09-05 23:00:31 -0500

    Otros metodos

--- modulos/consola.js
+++ modulos/consola.js
@@ -21,6 +21,24 @@ console.table(tabla);
 └─────────┴─────────┴──────────┘
 */
 
+const datos = [
+   { nombre: "Juan", edad: 30 },
+   { nombre: "Ana", edad: 25 },
+   { nombre: "Carlos", edad: 40 }
+ ];
+ 
+ console.table(datos);
+
+ /*
+ ─────────┬──────────┬──────┐
+│ (index) │  nombre  │ edad │
+├─────────┼──────────┼──────┤
+│    0    │  'Juan'  │  30  │
+│    1    │  'Ana'   │  25  │
+│    2    │ 'Carlos' │  40  │
+*/
+ 
+
 //** CONSOLE.GROUP */
 console.group("Numeros"); //Inicia el grupo
 console.log("1");
@@ -59,3 +77,20 @@ veces: 1
   veces: 2
   veces: 3
 */
+
+//!Limpiar la consola
+// console.clear();
+
+console.time("miTimer");
+// Realiza alguna operación que deseas medir
+//miTimer: 0.111ms
+console.timeEnd("miTimer");
+
+
+//! Evaluar expresiones
+
+const valor = 5;
+console.assert(valor === 10, "El valor no es igual a 10");
+
+
+
