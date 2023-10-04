b89b15325f1278f49007c626b7a12b4920bef091
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-10-03 22:26:34 -0500

    OS

---
+++ modulos/os.js
@@ -0,0 +1,18 @@
+//OS MODULE: operating system
+//Se utiliza para recuperar informacion del sistema operativo y sobre la máquina.
+
+const os = require('node:os');
+
+console.log(os.arch()) //-- x64 
+//visualizar la arquitectura
+
+console.log(os.platform()) //darwin
+//sistema operativo sobre el que estoy corriendo
+
+console.log(os.cpus())
+//Informacion de las cpus que tenemos en el sistema
+
+console.log(os.cpus().length)
+
+console.log(os.freemem()) //memoria libre que tenemos
+
